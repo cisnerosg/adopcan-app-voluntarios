729a8f18d361ed38ad6b65d8027078ffb9f60f6c
cisnerosgd <cisnerosgd@gmail.com>
2017-06-15 01:08:28 -0300

    agrego primera subida aplicacion

---
+++ app-voluntarios/app/src/main/java/com/adopcan/adopcan_voluntarios/MainMenuActivity.java
@@ -0,0 +1,15 @@
+package com.adopcan.adopcan_voluntarios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.adopcan.adopcan_voluntarios.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
